11511bba1a4ba2ff2caaec61b456bed6851333a7
sohyun-ku <sohyun.ku@nts-corp.com>
2025-03-18 18:21:37 +0900

    Add DDL script to create  call stack table

--- scavenger-schema/src/main/resources/db/changelog/schema-1.1.3.sql
+++ scavenger-schema/src/main/resources/db/changelog/schema-1.1.3.sql
@@ -3,3 +3,20 @@
 --changeset scavenger:6
 
 ALTER TABLE snapshot_nodes MODIFY COLUMN signature TEXT NOT NULL;
+
+
+--changeset scavenger:7
+CREATE TABLE IF NOT EXISTS call_stack
+(
+    id                  BIGINT AUTO_INCREMENT PRIMARY KEY,
+    customerId          BIGINT      NOT NULL,
+    applicationId       BIGINT      NOT NULL,
+    environmentId       BIGINT      NOT NULL,
+    signatureHash       VARCHAR(32) NOT NULL,
+    callerSignatureHash VARCHAR(32) NOT NULL,
+    invokedAtMillis     BIGINT      NOT NULL,
+    createdAt           TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+    CONSTRAINT ix_call_stack_identity
+        UNIQUE (customerId, applicationId, environmentId, signatureHash, callerSignatureHash)
+) COLLATE = utf8mb4_0900_as_cs;
