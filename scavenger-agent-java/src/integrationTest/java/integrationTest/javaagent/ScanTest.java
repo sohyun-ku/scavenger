7c68e2c81bac09ddd8cbc040313281953c46cf95
sohyun-ku <sohyun.ku@nts-corp.com>
2025-03-27 17:42:13 +0900

    Add exception handling

--- scavenger-agent-java/src/integrationTest/java/integrationTest/javaagent/ScanTest.java
+++ scavenger-agent-java/src/integrationTest/java/integrationTest/javaagent/ScanTest.java
@@ -64,8 +64,10 @@ public class ScanTest extends AbstractWireMockTest {
                 "sample.app.SampleAspect.logAspectLoaded()",
                 "sample.app.SampleService1(sample.app.SampleService2)",
                 "sample.app.SampleService1.doSomething(int)",
+                "sample.app.SampleService1.throwsException()",
                 "sample.app.SampleService2()",
-                "sample.app.SampleService2.doSomething(int)"
+                "sample.app.SampleService2.doSomething(int)",
+                "sample.app.SampleService2.throwsException()"
         );
     }
 
