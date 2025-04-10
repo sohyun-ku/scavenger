9e65aa15fd61605009cfeeef8eccbd0c8765b069
sohyun-ku <sohyun.ku@nts-corp.com>
2025-04-10 16:22:34 +0900

    Update grpcMock log level

--- scavenger-agent-java/build.gradle.kts
+++ scavenger-agent-java/build.gradle.kts
@@ -116,6 +116,7 @@ tasks.named<Test>("integrationTest") {
     systemProperty("integrationTest.scavengerAgent", tasks.shadowJar.get().outputs.files.asPath)
     systemProperty("integrationTest.classpath", "build/classes/java/integrationTest:$integrationTestRuntimeClasspath")
     systemProperty("integrationTest.javaPaths", javaPaths(8, 11, 17, 21))
+    systemProperty("org.slf4j.simpleLogger.log.org.grpcmock.GrpcMock", "error")
 }
 
 tasks.withType<ProcessResources> {
